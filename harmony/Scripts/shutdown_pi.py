778cda54e606b7b506af9730ef9cfe1d2ec43da8
Saurabh Datta <hi@dattasaurabh.com>
2019-05-10 00:59:43 +0800

    cleaning up install script for harmony board

--- harmony/Scripts/shutdown_pi.py
+++ harmony/Scripts/shutdown_pi.py
@@ -8,18 +8,17 @@ import os
  
 # Use the Broadcom SOC Pin numbers  
 # Setup the Pin with Internal pullups enabled and PIN in reading mode.  
-
-SHUT_PIN = 18
 GPIO.setmode(GPIO.BCM)  
-GPIO.setup(SHUT_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
  
 # Our function on what to do when the button is pressed  
 def Shutdown(channel):  
-    os.system("sudo shutdown -h now")
+    os.system("sudo shutdown -h now")  
  
 # Add our function to execute when the button pressed event happens  
-GPIO.add_event_detect(SHUT_PIN, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)  
+GPIO.add_event_detect(18, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)  
  
 # Now wait!  
 while 1:  
-    time.sleep(1) 
+    time.sleep(1)  
+ 
